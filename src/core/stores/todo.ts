7def8344e6a06fc84f14de3599a06469ea4e266a
kübra güçyeter <kubra.gucyeter01@gmail.com>
2024-07-23 10:00:53 +0300

    Yapılacak iş sayısı ekrana yazdırıldı.

--- src/core/stores/todo.ts
+++ src/core/stores/todo.ts
@@ -1,3 +1,10 @@
+
+interface Todo {
+  name: string
+  id: string
+  disabled: boolean
+}
+
 export const useTodoStore = defineStore('Todo Store', () => {
   const todoList = ref<{
     name: string
@@ -5,7 +12,7 @@ export const useTodoStore = defineStore('Todo Store', () => {
     disabled: boolean
 
   }[]>(JSON.parse(localStorage.getItem('todoList') || '[]'));
- 
+
   //   {
   //     name: 'Kediyi Besle',
   //     id: '1a7839b3-16a5-431f-8f19-087fd8838fe4',
@@ -14,11 +21,11 @@ export const useTodoStore = defineStore('Todo Store', () => {
   //   {
   //     name: 'Ödevini Yap',
   //     id: 'd6dee541-bf54-4d09-b9ef-9e5d683f8848',
-  //     disabled: true
+  //     disabled: truecv 
   //   }
-  
 
-  const todo = ref<string>(todoList.value[0])
+
+  const todo = ref<Todo | undefined>(todoList.value[0])
 
   const setTodo = (todo: string) => {
     if (!todo.trim()) return; // boş todo engelleme
@@ -27,12 +34,12 @@ export const useTodoStore = defineStore('Todo Store', () => {
       id: Math.random().toString(36).substr(2, 9),
       disabled: true
     };
-    
+
     todoList.value.push(newTodo)
     localStorage.setItem('todoList', JSON.stringify(todoList.value))
   }
 
-  // index kısmında seçme komutu ayarlanacak 'ayarlandı' 
+
   const removeTodo = (id: string) => {
     todoList.value = todoList.value.filter(todo => todo.id !== id)
   };
@@ -41,7 +48,7 @@ export const useTodoStore = defineStore('Todo Store', () => {
     todoList.value = [];
     localStorage.setItem('todoList', JSON.stringify(todoList.value));
   };
- 
+
   watch(todoList, (newTodoList) => {
     localStorage.setItem('todoList', JSON.stringify(newTodoList));
   }, { deep: true });
--- src/views/pages/projects/index.vue
+++ src/views/pages/projects/index.vue
@@ -34,14 +34,21 @@ const handleSave = () => {
   }
 };
 
+const pendingTasks = computed(() => {
+  return todoStore.todoList.filter((task) => task.disabled);
+});
+
 
 </script>
 
 <template>
   <div class="w-full gap-3 flex flex-column align-items-center  justify-content-center">
+
     <div v-for="todo in todoStore.todoList" :key="todo.id">
-      <div
-        class="flex align-items-center justify-content-between gap-2 w-30rem border-1 border-round border-gray-400 py-2 px-3 ">
+      <div @click="() => {
+        todoStore.todo = todoStore.todoList.find(o => o.id === todo.id)
+      }"
+        class="flex cursor-pointer hover:border-blue-400 hover:text-blue-400 align-items-center justify-content-between gap-2 w-30rem border-1 border-round border-gray-400 py-2 px-3 ">
         <div class="flex gap-2">
           <div :class="`pi ${todo.disabled ? 'pi-circle-fill text-green-400' : 'pi-circle text-red-400'}`" />
           <label> {{ todo.name }} </label>
@@ -50,13 +57,24 @@ const handleSave = () => {
           <Button @click="changeDisable(todo.id)" link icon="pi pi-check-circle" />
           <Button @click="todoStore.removeTodo(todo.id)" link icon="pi pi-times-circle" />
         </div>
+
+
+
+
       </div>
     </div>
     <div class="flex flex-column gap-2 w-20rem">
+      <div v-if="pendingTasks.length === 0">
+        yapılacak iş yoktur.
+      </div>
+      <div v-else>
+        <div> {{ pendingTasks.length }} tane iş yapılacak.</div v-if="">
+      </div>
       <InputText v-model="todo" @keyup.enter="handleSave" />
       <Button label="save" icon="pi pi-save" @click="handleSave" />
-      <Button label="clear" icon="pi pi-trash " @click="todoStore.clearTodo(todoList)" />
+      <Button label="clear" icon="pi pi-trash " @click="todoStore.clearTodo" />
     </div>
+
     <div>
       <Message v-if="showWarning" severity="warn">{{ warningMessage }}</Message>
 
